59cf6ccf394e7462b909062f5c5e7ce38ec43886
pwharned <pwharned@gmail.com>
2022-06-08 11:11:33 -0700

    updated namespace target

---
+++ 0-bootstrap/single-cluster/2-services/argocd/instances/execute.sh
@@ -0,0 +1,1 @@
+curl -X POST -d '{"team":"dse", "format":"JSON"}' -H 'Content-type: application/json' https://us-south.functions.appdomain.cloud/api/v1/web/DSE-apps_DSE-Dashboard/isc_test_actions/opportunities_act
